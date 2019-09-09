e9a30140ab2a0c906b5de361976345320562d896
Sergiy Rudenko <sergiy@powertree.io>
2019-09-09 09:01:39 -0400

    added components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header'
-import SearchPanel from './components/search-panel'
-import TodoList from './components/todo-list'
+import App from './components/app';
 
-const App = () => {
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>
-    )
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
